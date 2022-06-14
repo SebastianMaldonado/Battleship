3e268a4e145bd00f5fe54c4f734bb1893a1d7b13
SebastianMaldonado <98917583+SebastianMaldonado@users.noreply.github.com>
2022-06-14 17:43:38 -0500

    Add files via upload

---
+++ src/battleship/Tablero.java
@@ -0,0 +1,10 @@
+package battleship;
+
+public class Tablero {
+    public void imprimirTab(int matriz[][]) {
+        for (int i = 0; i <= 7; i++) {
+            for (int j = 0; j <= 7; j++) {
+                            }
+        }
+    }
+}
