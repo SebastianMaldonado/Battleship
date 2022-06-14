73da4042e226a887ae0ff89cab7358ad02f33dab
SebastianMaldonado <98917583+SebastianMaldonado@users.noreply.github.com>
2022-06-14 17:44:41 -0500

    Add files via upload

--- src/battleship/Tablero.java
+++ src/battleship/Tablero.java
@@ -4,7 +4,16 @@ public class Tablero {
     public void imprimirTab(int matriz[][]) {
         for (int i = 0; i <= 7; i++) {
             for (int j = 0; j <= 7; j++) {
-                            }
+                if (matriz[i][j] == 0) {
+                    System.out.println("- ");
+                } else if (matriz[i][j] == 1) {
+                    System.out.println("+ ");
+                } else if (matriz[i][j] == 2) {
+                    System.out.println("* ");
+                }
+            }
         }
     }
+
+
 }
